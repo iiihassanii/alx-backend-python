1213b49fb9a280284a5695c6c1b17e0ea89c66a1
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-06 13:00:42 +0300

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import math
+"""_summary_
+    """
+
+
+def floor(n: float) -> int:
+    """_summary_
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        int: _description_
+    """
+    return math.floor(n)
