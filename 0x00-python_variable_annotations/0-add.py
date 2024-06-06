a315e314b27e97ff96e123861a9ef31d35677f8f
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-06 12:54:19 +0300

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""_summary_
+    """
+
+
+def add(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return a+b
