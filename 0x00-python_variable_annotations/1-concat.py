c18ca8f646aeac61a521492a3926ce8d6cb7b1e1
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-06 12:57:38 +0300

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""_summary_
+    """
+
+
+def concat(str1: str, str2: str) -> str:
+    """_summary_
+
+    Args:
+        str1 (str): _description_
+        str2 (str): _description_
+
+    Returns:
+        str: concatenated string
+    """
+    return str1+str2
