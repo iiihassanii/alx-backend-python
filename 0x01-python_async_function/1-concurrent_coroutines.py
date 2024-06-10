908350f726fb2cff4dd6e4c0d4c40ea71cf4d968
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-10 12:52:47 +0300

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,11 +2,19 @@
 """_summary_
     """
 
-import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> float:
+    """_summary_
+
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
+
+    Returns:
+        float: _description_
+    """
     delays = []
     for _ in range(n):
         delays.append(await wait_random(max_delay))
