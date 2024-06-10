2043482fd793cb22899645473dbe8321656869c0
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-10 13:33:03 +0300

    task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""_summary_
+    """
+
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """_summary_
+
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
+
+    Returns:
+        float: _description_
+    """
+    delays = []
+
+    if n == 0:
+        return delays
+    for _ in range(n):
+        delays.append(await wait_random(max_delay))
+    delays.sort()
+    return delays
