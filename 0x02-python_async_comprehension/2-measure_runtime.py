cc86346ac97ef273ddffaeee84e0510a530f846c
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-11 13:10:58 +0300

    task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,7 +9,7 @@ import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """_summary_
     """
     start_time = time.perf_counter()
