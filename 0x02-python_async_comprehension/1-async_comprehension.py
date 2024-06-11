0fa491d3cdd23f041037304a85a271e23a353f00
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-11 12:56:27 +0300

    task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""_summary_
+
+    Yields:
+        _type_: _description_
+    """
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
